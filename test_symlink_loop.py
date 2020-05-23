47725f759f7dae380fbc780b7719d9983d1855a0
Hiroshi Miura <miurahr@linux.com>
2020-05-23 13:10:07 +0900

    first import
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ test_symlink_loop.py
@@ -0,0 +1,31 @@
+import pathlib
+
+import pytest
+
+
+def walk(path, dereference=True):
+    if path.is_symlink() and not dereference:
+        print(path)
+    elif path.is_file():
+        print(path)
+    elif path.is_dir():
+        if not path.samefile('.'):
+            print(path)
+        for nm in sorted(os.listdir(str(path))):
+            walk(path.joinpath(nm))
+
+
+
+def test_symlink_deep_loop(tmp_path):
+    origin = tmp_path / 'src' / 'original.txt'
+    origin.parent.mkdir(parents=True, exist_ok=True)
+    with origin.open('w') as f:
+        f.write("Original")
+    slink = tmp_path / "src" / "link"
+    slink.parent.mkdir(parents=True, exist_ok=True)
+    target = pathlib.Path('../parent/original.txt')
+    slink.symlink_to(target, False)
+    plink = tmp_path / "src" / "parent"
+    plink.symlink_to(tmp_path / "src", False)
+    with pytest.raises(OSError):
+        walk(tmp_path / 'src')
